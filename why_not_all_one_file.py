b33f5cf7dbc786a390fb0cf6facf4919d0fca048
ty <tailstylerturnquist@gmail.com>
2025-04-26 18:35:05 -0500

    .

--- why_not_all_one_file.py
+++ why_not_all_one_file.py
@@ -233,3 +233,21 @@ class Solution:
                 print(s[0], " != ", s[-1])
                 return False
         return True
+
+
+
+    class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        front = 0
+        back = len(numbers) - 1
+        while front != back:
+            if numbers[front] + numbers[back] == target:
+                return [front + 1, back + 1]
+            elif numbers[front] + numbers[back] < target:
+                front += 1
+            else:
+                back -= 1
+        return "error"
+
+
+
