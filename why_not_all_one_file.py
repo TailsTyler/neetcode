5f49e2d6f022c8a198cab1327cca3f1fd3a92c1c
ty <tailstylerturnquist@gmail.com>
2025-04-26 00:01:05 -0500

    .

--- why_not_all_one_file.py
+++ why_not_all_one_file.py
@@ -170,11 +170,11 @@ class Solution:
 
 
 
-
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         note = {}
+        if len(nums) == 0:
+            return 0
         smallest = nums[0]
         biggest = nums[0]
         for n in nums:
@@ -190,9 +190,13 @@ class Solution:
         ans = 0
         current_run = 0
         last_one_was_1_less = False
-        for i in range(smallest, biggest):
+        for i in range(smallest, biggest + 1):
+            if ans > biggest + 1 - i + current_run:
+                print("made sense to stop short")
+                return ans
             if i in note:
                 current_run += 1
+                print(i, " in note so current_run now ", current_run)
                 last_one_was_1_less = True
             else:
                 if current_run > ans:
@@ -202,6 +206,5 @@ class Solution:
         if current_run > ans:
             ans = current_run
         return ans
-
 
 
