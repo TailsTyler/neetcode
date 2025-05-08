cba2e908888bfe8c035513042b2c96fa6ac6b33b
ty <tailstylerturnquist@gmail.com>
2025-05-07 23:38:48 -0500

    .

--- why_not_all_one_file.py
+++ why_not_all_one_file.py
@@ -274,3 +274,29 @@ class Solution:
             print("i: ", i)#, " which has a ", nums[i])
         return ans
 
+
+
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l = 0
+        r = len(heights) - 1
+        area = 0
+        while True:
+            d = r - l
+            new_area = min(heights[l], heights[r]) * d
+            if new_area > area:
+                area = new_area
+
+                print("area is now ", area, " since heights[l] == ", heights[l], " and heights[r] == ", heights[r], " and the distance is ", d)
+            if heights[l] < heights[r]:
+                l+=1
+                print("left is now ", heights[l])
+            else:
+                r-=1
+                print("right is now ", heights[r])
+            if l == r:
+                break
+        return area
+
