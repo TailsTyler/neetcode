301f1624cb67cc248ce5859e7b887fa972552268
ty <tailstylerturnquist@gmail.com>
2025-06-14 12:57:54 +0000

    .

--- debug.py
+++ debug.py
@@ -1,3 +1,70 @@
+
+
+class Solution:
+    @staticmethod
+    def count_water(l, r, height) -> int:
+        if l > 0 and r > 0:
+            d = r - l - 1
+            print("d = ", d)
+        else:
+            return 0
+        if r > l:
+            rectangle = height[l] * d
+            print("rectangle = height[l] * d = ", height[l], " * ", d, " = ", height[l] * d)
+        else:
+            rectangle = height[r] * d
+            print("rectangle = height[r] * d = ", height[r], " * ", d, " = ", height[r] * d)
+        stuff_in_between = 0
+        for i in range(l + 1, r):
+            stuff_in_between += height[i]
+            print("height[", i, "] brings stuff_in_between to ", stuff_in_between)
+        print("new water = rectangle - stuff_in_between = ", rectangle, " - ", stuff_in_between)
+        return rectangle - stuff_in_between
+    #for passing all the way thru the first time, then backwarks to stop
+    def pass(self) -> None:
+        if not stop:
+            back = 0 #index of back, first-reached-in-this-pass side of a possible pool. no stop so this is the first, full pass
+        else:
+            back = len(height) - 1 #this is the second, backwards pass to the stop point
+        for i, h in enumerate(height):
+            if h > 0:
+                l = i
+                print("l = ", back)
+                break
+        front = l + 1 #index of current spot being considered for the front side of a pool, ie the side that is new and farther in the direction of the pass
+        t = front # index of rightmost tallest found so far to the right of l. Used to find pools when there is no wall as high as height[l]
+        while front != len(height) - 1:
+            if height[front] >= height[t]:
+                print("height[front] >= height[t]:")
+                t = r
+            if front > l + 1 and height[front] >= height[l]: #found the right side of a pool because it is as high as the left
+                print("counting water with l = ", l, " and front = ", front)
+                water += Solution.count_water(l, t, height)
+                print("water == ", water)
+                #reset values because now searching for next pool
+                t = front
+                l = front
+            else:
+                front += 1
+                print("front == ", front)
+                print("l == ", l)
+                print("t == ", t)
+                print('\n')
+        #go backwards to highest point
+        if not stop:
+            pass(self)
+            
+            
+    def trap(self, height: list[int]) -> int:
+        self.stop = None #where to stop early on the second, backwards pass, at the high point
+        self.water = 0 #water found
+        self.front =
+        return water
+#height = [0,2,0,3,1,0,1,3,2,1]
+height=[0,1,0,2,1,0,1,3,2,1,2,1]
+sol = Solution()
+print(sol.trap(height))
+
 '''
 Wrong Answer
 
@@ -96,78 +163,3 @@ Expected output:
 
 6
 '''
-
-class Solution:
-    @staticmethod
-    def count_water(l, r, height) -> int:
-        if l > 0 and r > 0:
-            d = r - l - 1
-            print("d = ", d)
-        else:
-            return 0
-        if r > l:
-            rectangle = height[l] * d
-            print("rectangle = height[l] * d = ", height[l], " * ", d, " = ", height[l] * d)
-        else:
-            rectangle = height[r] * d
-            print("rectangle = height[r] * d = ", height[r], " * ", d, " = ", height[r] * d)
-        stuff_in_between = 0
-        for i in range(l + 1, r):
-            stuff_in_between += height[i]
-            print("height[", i, "] brings stuff_in_between to ", stuff_in_between)
-        print("new water = rectangle - stuff_in_between = ", rectangle, " - ", stuff_in_between)
-        return rectangle - stuff_in_between
-    def trap(self, height: list[int]) -> int:
-        l = 0 #index of left side of a possible pool
-        for i, h in enumerate(height):
-            if h > 0:
-                l = i
-                print("l = ", l)
-                break
-        r = 0 #index of current spot being considered for the right side of a pool
-        t = 0 # index of rightmost tallest found so far to the right of l. Used to find pools when there is no wall as high as height[l]
-        water = 0 #water found
-        while r != len(height) - 1:
-            if height[r] >= height[t]:
-                print("height[r] >= height[t]:")
-                t = r
-            if r > l + 1 and height[r] >= height[l]: #found the right side of a pool because it is as high as the left
-                print("counting water with l = ", l, " and r = ", r)
-                water += Solution.count_water(l, t, height)
-                print("water == ", water)
-                #reset values because now searching for next pool
-                t = r
-                l = r
-            else:
-                r += 1
-                print("r == ", r)
-                print("l == ", l)
-                print("t == ", t)
-                print('\n')
-        #go backwards to highest point
-        stop = t
-        print("turning around. stop = ", stop)
-        l = r - 1
-        t = r
-        while l != stop - 1:
-            if height[l] >= height[t]:
-                print("height[l] >= height[t]:")
-                t = l
-            if l > r + 1 and height[l] >= height[r]: #found the left side of a pool because it is as high as the right
-                print("counting water with r =", r, " and l =", l)
-                water += Solution.count_water(r, t, height)
-                print("water == ", water)
-                #reset values because now searching for next pool
-                t = l
-                r = l
-            else:
-                l -= 1
-                print("l == ", l)
-                print("r == ", r)
-                print("t == ", t)
-                print('\n')
-        return water
-#height = [0,2,0,3,1,0,1,3,2,1]
-height=[0,1,0,2,1,0,1,3,2,1,2,1]
-sol = Solution()
-print(sol.trap(height))
