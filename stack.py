281dfd8569ce2b1ebcca5548d6b9d1c0bc4612f2
ty <tailstylerturnquist@gmail.com>
2025-06-22 20:37:10 -0500

    .

--- stack.py
+++ stack.py
@@ -66,3 +66,33 @@ class MinStack:
             return None
         else:
             return self.mins[-1]
+        
+
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        i = 0
+        nums = []
+        while tokens != []:
+            try:
+                nums.append(int(tokens[0]))
+                print("nums increased to ", nums)
+            except:
+                op = tokens[0]
+                if op == '+':
+                    nums[-2] = nums[-2] + nums[-1]
+                elif op == '-':
+                    nums[-2] = nums[-2] - nums[-1]
+                elif op == '*':
+                    nums[-2] = nums[-2] * nums[-1]
+                elif op == '/':
+                    nums[-2] = int(nums[-2] / nums[-1])
+                else:
+                    print('error 1')
+                nums = nums[0:-1]
+                print("op ", op, " occured, making nums ", nums)
+            tokens = tokens[1:]
+        if len(nums) != 1:
+            print("error: len(nums) = ", len(nums))
+        return nums[0]
+    sol = Solution()
+    print(sol.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
