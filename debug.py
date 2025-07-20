0f4d9d927608a2f756a52d546667f3b6579da73b
ty <tailstylerturnquist@gmail.com>
2025-07-20 17:54:56 -0500

    now you can add your arg durectly to terminal like  python3 debug.py 1

--- debug.py
+++ debug.py
@@ -1,29 +1,63 @@
+import sys
+
+
 class Solution:
-    def isValid(self, s: str) -> bool:
-        opens = '({['
-        closes = ')}]'
-        print(s)
-        collection = []
-        i = 0
-        while i < len(s) - 1:
-            try:
-                open = opens.index(s[i])
-            except:
-                open = None
-            if open:
-                collection.append(open)
+    def is_nested(self, s):
+        print("checking if nested: ", s)
+        while len(s) > 0:
+            if s[0:2] == '()':
+                s = s[2:]
+                print("left to check: ", s)
             else:
-                try:
-                    if collection[-1] == closes.index(s[i]):
-                        collection = collection[1:]
-                    else:
-                        print("a")
-                        return False
-                except:
-                    return False
-                    print("b")
-            i+=1
+                return False
         return True
-s="([{}])"
-sol = Solution()
-print(sol.isValid(s))
+    def generateParenthesis(self, n: int) -> list[str]:
+        ans = ['()']
+        while n > 1:
+            for i in range(len(ans)):
+                ans.append(ans[i] + "()")
+                print("ans[-1]: ", print(ans[-1]))
+                if not self.is_nested(ans[-1]):
+                    print("here")
+                    potential = "()" + ans[i]
+                    try:
+                        if ans.index(potential) == -1:
+                            pass
+                    except:
+                        ans.append(potential)
+                ans[i] = "(" + ans[i] + ")"
+                print("ans: ", ans)
+            n-=1
+        return ans
+
+s = Solution()
+n = int(sys.argv[1])  # gets the first argument after the script name
+print(s.generateParenthesis(n))
+
+            
+'''
+if 1 retun () else return prev except for each one, (.), .+(), and if .+() was not sym, then ()+., too 
+
+
+Your Output:
+
+["(((())))","((()()))","((())())","(()(()))","(()()())","((()))()","()((()))","(()())()","()(()())","(())()()","()(())()","()(())()","()()(())","()()()()"]
+
+Expected output:
+
+["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]
+
+['(((())))', '((()()))', '((())())', '(()()())', '((()))()', '(()())()', '(())()()', '()()()()']
+
+----
+
+Input: n = 3
+
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+['((()))', '(()())', '(())()', '()()()']
+
+
+
+
+'''
+        
