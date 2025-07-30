fb639ffbbf893ad0fb020f48ef06110daa240351
ty <tailstylerturnquist@gmail.com>
2025-07-30 10:56:31 -0500

    .

--- debug.py
+++ debug.py
@@ -4,33 +4,34 @@ import sys
 import unittest
 
 class Solution:
-    def delete(self, i):
-        self.position.pop(i)
-        self.speed.pop(i)
+
+    def delete(self, i, position, speed):
+        position.pop(i)
+        speed.pop(i)
     def is_not_sorted(self, lst):
         return any(lst[i] > lst[i+1] for i in range(len(lst) - 1))
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
-        paired = list(zip(self.position, self.speed))
+        paired = list(zip(position, speed))
         paired.sort()   
-        self.position, self.speed = zip(*paired)
-        self.position = list(self.position)
-        self.speed = list(self.speed)
+        position, speed = zip(*paired)
+        position = list(position)
+        speed = list(speed)
         arrived = 0 #fleets that have made it to the target
 
         while self.is_not_sorted(speed): #if the speeds are a sorted list, cars will never each each other
             #advance all fleets forward by their speed amount
-            for i in reversed(range(len(self.position))):
-                self.position[i] += self.speed[i]
+            for i in reversed(range(len(position))):
+                position[i] += speed[i]
                 #passed target
-                if self.position[i] > target:
-                    self.delete(i)
+                if position[i] > target:
+                    self.delete(i, position, speed)
                     arrived += 1
             #check for collisions
-            for i in reversed(range(len(self.position) - 1)):
+            for i in reversed(range(len(position) - 1)):
                 #collision
-                if self.position[i] >= self.position[i+1]:
-                    self.delete(i) #delete the one that used to be in the back cuz it is faster, and fleets go at the lower speed
-        return arrived + len(self.position)
+                if position[i] >= position[i+1]:
+                    self.delete(i, position, speed) #delete the one that used to be in the back cuz it is faster, and fleets go at the lower speed
+        return arrived + len(position)
                           
 
         # Unzip
@@ -57,35 +58,16 @@ print(x.carFleet(t, p, s))
 '''
 Input:
 
-temperatures=
-
-stdout:
-
-[30, 38, 30, 36, 35, 40, 28]
-[0, 0, 0, 0, 0, 0, 0]
-high:  30
-ans[i - 1] = 1:  [1, 0, 0, 0, 0, 0, 0]
-i[2]: 3
-high:  35
-ans[i - 1] = 1:  [1, 0, 1, 0, 0, 0, 0]
-ans[1]:  [1, 2, 1, 0, 0, 0, 0]
-ans[1]:  [3, 2, 1, 0, 0, 0, 0]
-i[2]: 5
-high:  28
-ans[i - 1] = 1:  [3, 2, 1, 0, 1, 0, 0]
-ans[1]:  [3, 2, 1, 2, 1, 0, 0]
-ans[1]:  [3, 2, 3, 2, 1, 0, 0]
-ans[1]:  [3, 4, 3, 2, 1, 0, 0]
-ans[1]:  [5, 4, 3, 2, 1, 0, 0]
-i[2]: 7
+target=16
+position=[11,14,13,6]
+speed=[2,2,6,7]
 
 Your Output:
 
-[5,4,3,2,1,0,0]
+3
 
 Expected output:
 
-[1,4,1,2,1,0,0]
-
+2
 '''
         
