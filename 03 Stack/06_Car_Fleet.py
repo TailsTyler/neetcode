f77881bf528a1adc98e765133cdb7df78ecca8ed
ty <tailstylerturnquist@gmail.com>
2025-08-02 14:45:43 -0500

    .

--- 03 Stack/06_Car_Fleet.py
+++ 03 Stack/06_Car_Fleet.py
@@ -1,3 +1,83 @@
 class Solution:
-    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+    def is_not_sorted(self, lst):
+        return any(lst[i] > lst[i+1] for i in range(len(lst) - 1))
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        paired = list(zip(position, speed))
+        paired.sort()   
+        position, speed = zip(*paired)
+        position = list(position)
+        speed = list(speed)
+        right_completion_time = (target - position[-1]) / speed[-1]
+        for i in reversed(range(len(position) - 1)):
+            left_completion_time = (target - position[i]) / speed[i]
+            #remove the faster cars in collisions
+            if left_completion_time <= right_completion_time:
+                position.pop(i)
+                speed.pop(i)
+            else:
+                right_completion_time = left_completion_time
+        return len(position)
         
+
+
+
+
+
+        '''
+
+        old solution before watching video that was convoluted but only failed on advanced tests like the one below
+
+
+        class Solution:
+
+    def delete(self, i, position, speed):
+        position.pop(i)
+        speed.pop(i)
+    def is_not_sorted(self, lst):
+        return any(lst[i] > lst[i+1] for i in range(len(lst) - 1))
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        paired = list(zip(position, speed))
+        paired.sort()   
+        position, speed = zip(*paired)
+        position = list(position)
+        speed = list(speed)
+        arrived = 0 #fleets that have made it to the target
+
+        while self.is_not_sorted(speed): #if the speeds are a sorted list, cars will never each each other
+            #advance all fleets forward by their speed amount
+            for i in reversed(range(len(position))):
+                position[i] += speed[i]
+            #check for collisions
+            for i in reversed(range(len(position) - 1)):
+                #collision
+                if position[i] >= position[i+1]:
+                    self.delete(i, position, speed) #delete the one that used to be in the back cuz it is faster, and fleets go at the lower speed
+            #remove any that have reached the target, now that collisions are dealt with
+            for i in reversed(range(len(position))):
+                if position[i] >= target:
+                    self.delete(i, position, speed)
+                    arrived += 1
+                else:
+                    break
+        return arrived + len(position)
+'''
+'''
+[5, 6, 12, 18, 19, 21, 22, 25, 26, 29]
+13  10 19  24  25  25  31  29  32  32 
+[7, 4,  7,  6,  6,  4,  9,  4,  6,  3]
+Input:
+
+target=31
+position=[5,26,18,25,29,21,22,12,19,6]
+speed=[7,6,6,4,3,4,9,7,6,4]
+
+Your Output:
+
+4
+
+Expected output:
+
+6
+
+'''
--- debug.py
+++ debug.py
@@ -5,9 +5,6 @@ import unittest
 
 class Solution:
 
-    def delete(self, i, position, speed):
-        position.pop(i)
-        speed.pop(i)
     def is_not_sorted(self, lst):
         return any(lst[i] > lst[i+1] for i in range(len(lst) - 1))
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
@@ -16,22 +13,16 @@ class Solution:
         position, speed = zip(*paired)
         position = list(position)
         speed = list(speed)
-        arrived = 0 #fleets that have made it to the target
-
-        while self.is_not_sorted(speed): #if the speeds are a sorted list, cars will never each each other
-            #advance all fleets forward by their speed amount
-            for i in reversed(range(len(position))):
-                position[i] += speed[i]
-                #passed target
-                if position[i] > target:
-                    self.delete(i, position, speed)
-                    arrived += 1
-            #check for collisions
-            for i in reversed(range(len(position) - 1)):
-                #collision
-                if position[i] >= position[i+1]:
-                    self.delete(i, position, speed) #delete the one that used to be in the back cuz it is faster, and fleets go at the lower speed
-        return arrived + len(position)
+        right_completion_time = (target - position[-1]) / speed[-1]
+        for i in reversed(range(len(position) - 1)):
+            left_completion_time = (target - position[i]) / speed[i]
+            #remove the faster cars in collisions
+            if left_completion_time <= right_completion_time:
+                position.pop(i)
+                speed.pop(i)
+            else:
+                right_completion_time = left_completion_time
+        return len(position)
                           
 
         # Unzip
@@ -41,33 +32,34 @@ class Solution:
 
 # Output: 3
 
-t = 10
-p = [4,1,0,7]
-''' [6,3,1,8]
-    [8,5,2,9] 
-    [10,7,3,10]'''
-s = [2,2,1,1]
+
+target=31
+position=[5,26,18,25,29,21,22,12,19,6]
+speed=[7,6,6,4,3,4,9,7,6,4]
 
 x = Solution()
-print(x.carFleet(t, p, s))
+print(x.carFleet(target, position, speed))
 
 # if __name__ == "__main__":
 #     unittest.main()
 
             
 '''
+[5, 6, 12, 18, 19, 21, 22, 25, 26, 29]
+13  10 19  24  25  25  31  29  32  32 
+[7, 4,  7,  6,  6,  4,  9,  4,  6,  3]
 Input:
 
-target=16
-position=[11,14,13,6]
-speed=[2,2,6,7]
+target=31
+position=[5,26,18,25,29,21,22,12,19,6]
+speed=[7,6,6,4,3,4,9,7,6,4]
 
 Your Output:
 
-3
+4
 
 Expected output:
 
-2
+6
 '''
         
